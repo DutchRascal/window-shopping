e11f23bec03cdcbdc3b7ca66019aa5e2a33f5903
DutchRascal <Andre@iMac.fritz.box>
2017-08-02 20:01:21 +0200

    lesson-01

---
+++ window-shopping/window-shopping/Controller/MainVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  window-shopping
+//
+//  Created by Andre Boevink on 02/08/2017.
+//  Copyright © 2017 Andre Boevink. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
+
