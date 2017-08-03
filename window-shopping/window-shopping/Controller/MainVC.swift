474d6c654619e153a936994e56c7a0031920ca11
DutchRascal <Andre@iMac.fritz.box>
2017-08-03 18:42:15 +0200

    lesson-04

--- window-shopping/window-shopping/Controller/MainVC.swift
+++ window-shopping/window-shopping/Controller/MainVC.swift
@@ -12,6 +12,8 @@ class MainVC: UIViewController
 {
     @IBOutlet weak var wageTxt: CurrencyTxtField!
     @IBOutlet weak var priceTxt: CurrencyTxtField!
+    @IBOutlet weak var resultLbl: UILabel!
+    @IBOutlet weak var hoursLbl: UILabel!
     
     override func viewDidLoad()
     {
@@ -24,11 +26,30 @@ class MainVC: UIViewController
         calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
         wageTxt.inputAccessoryView = calcBtn
         priceTxt.inputAccessoryView = calcBtn
+        resultLbl.isHidden = true
+        hoursLbl.isHidden = true
     }
     
     @objc func calculate()
     {
-        print("We got here")
+        if let wageTxt = wageTxt.text, let priceTxt = priceTxt.text
+        {
+            if let wage = Double(wageTxt), let price = Double(priceTxt)
+            {
+                view.endEditing(true)
+                resultLbl.isHidden = false
+                hoursLbl.isHidden = false
+                resultLbl.text = "\(Wage.getHours(forWage: wage, andPrice: price))"
+            }
+        }
+    }
+    
+    @IBAction func clearCalculatorPressed(_ sender: UIButton)
+    {
+        resultLbl.isHidden = true
+        hoursLbl.isHidden = true
+        wageTxt.text = ""
+        priceTxt.text = ""
     }
 }
 
